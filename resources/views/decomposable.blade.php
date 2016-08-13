f74c6fd396a2fb5d69594e62cdc2663862913b14
Anugrahaa <vr.anugrahaa@gmail.com>
2016-08-13 11:26:01 +0530

    First commit

---
+++ resources/views/decomposable.blade.php
@@ -0,0 +1,7 @@
+<html>
+	<body>
+		<div>
+			<input type = "text" placeholder = "product">
+		</div>
+	</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+    <head>
+<title>IncredibleNITT| Home</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Inventor Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+          
+</head>
+    <body>
+        <div class="row" style="height:80%; margin-top:10%;">
+            <div class="col m1"></div>
+        <div class="col m4">
+
+            <div class="card medium">
+              <div class="card-image">
+              <img src="{{ asset('home_images/decompose.jpg') }}" alt="Card image">
+              <span class="card-title">Card Title</span>
+                </div>
+              <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <a href="decomposable" class="card-action">
+              Find out if your product is biodegradable
+            </a>
+            </div>
+            </div>
+            <div class="col m2"></div>
+            <div class="col m4">
+
+            <div class="card medium">
+              <div class="card-image">
+              <img src="{{ asset('home_images/decompose.jpg') }}" alt="Card image">
+              <span class="card-title">Card Title</span>
+                </div>
+              <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+            </div>
+            </div>
+            </div>
+    </div>
+        <script src="{{  asset('packages/home/js/bootstrap.js') }}"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+
+    </body>
+</html>
